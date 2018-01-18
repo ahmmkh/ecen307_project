7eaf024326a57986a51aab0472f896b8c0ba59f6
Unknown <yomnah_1996@hotmail.com>
2018-01-18 15:17:05 +0200

    dds

--- digger.hpp
+++ digger.hpp
@@ -2,6 +2,6 @@
 #ifndef __DIGGER_H_INCLUDED__   
 #define __DIGGER_H_INCLUDED__  
 
-/** TODO implement digger class for using diggingstrategy object and terminal_pattern(sudoko board)
+/** TODOs implement digger class for using diggingstrategy object and terminal_pattern(sudoko board)
  **/ 
 #endif 
