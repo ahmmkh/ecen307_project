0a3628ea3ac43828cb6e267c0768b5ac7fdea5bd
AHmed Khatab <b-b@dr.com>
2018-01-19 21:44:20 +0200

    added a filler function

--- la.hpp
+++ la.hpp
@@ -1,4 +1,12 @@
 #ifndef __LA_H_INCLUDED__   
 #define __LA_H_INCLUDED__  
+#include "board.hpp"
+#include "solve.hpp"
+#include <vector>
+Board * backtrack(Board* b,std::vector<int> set,int pos){
+
+	
+}
+
 //TODO GENERATE A SOLVED PUZZLE BOARD USING SOLVE FUNCTION 
 #endif 
